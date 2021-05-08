349b4b7dcfde35c79b310293232680846a37d74c
Cat-Denton <willwdenton@gmail.com>
2021-05-07 17:38:10 -0700

    test written and passed for Vendor constructor

---
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorAndOrderTracker.Models;
+using System;
+
+namespace VendorAndOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    // public void Dispose()
+    // {
+    //   Vendor.ClearAll();
+    // }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("testName","testDescript");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
