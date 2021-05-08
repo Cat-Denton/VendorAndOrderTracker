e9f2573b9149874d21522611510975643bcab8e3
Cat-Denton <willwdenton@gmail.com>
2021-05-07 17:29:10 -0700

    add public properties, private _instances field and constructor to Vendor class

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -4,6 +4,17 @@ namespace VendorAndOrderTracker.Models
 {
   public class Vendor
   {
-    
+    public string VendorName { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Vendor> _instances = new List<Vendor> { };
+
+    public Vendor(string vendorName, string description)
+    {
+      VendorName = vendorName;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
   }
 }
