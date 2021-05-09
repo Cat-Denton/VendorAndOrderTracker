49e22f8451a31d3b9ce6f8bd8de89340564292eb
Cat-Denton <willwdenton@gmail.com>
2021-05-08 17:13:59 -0700

    add constructor test

---
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorAndOrderTracker.Models;
+using System;
+
+namespace VendorAndOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("title","description",0,"date");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
