6121146f027d6c09082fff8341782c2ff7431f9c
Cat-Denton <willwdenton@gmail.com>
2021-05-08 17:04:40 -0700

    add ClearAll method

--- VendorAndOrderTracker/Models/Order.cs
+++ VendorAndOrderTracker/Models/Order.cs
@@ -11,5 +11,19 @@ namespace VendorAndOrderTracker.Models
     public bool IsClosed { get; set; }
     private static List<Order> _instances = new List<Order> { };
     public int Id { get; }
+
+    public Order(string title, string description, int price, string date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
